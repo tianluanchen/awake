337e4ac05f87ae9cbc434eaa089f4d857e3da0c0
Ayouth <75252550+tianluanchen@users.noreply.github.com>
2024-11-25 02:07:24 +0000

    Switch to using the --output flag

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -1,9 +1,9 @@
 package cmd
 
 import (
+	"crypto/x509"
 	"awake/fetch"
 	"awake/pkg"
-	"crypto/x509"
 
 	"fmt"
 	"io"
@@ -18,30 +18,30 @@ import (
 var fetchCmd = &cobra.Command{
 	Use:     "fetch",
 	Short:   "Concurrent download of web content to local",
-	Example: "fetch <url> <output file?>",
-	Args:    cobra.RangeArgs(1, 2),
+	Example: "fetch <url> -o <output>",
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		output, _ := cmd.Flags().GetString("output")
 		u, err := pkg.ParseURL(args[0])
 		if err != nil {
 			logger.Fatalln(err)
 		}
-		args[0] = u.String()
-		if len(args) == 1 {
+		target := u.String()
+		if output == "" {
 			if name, err := pkg.ParseFileNameByURLPath(u.Path); err != nil {
-				logger.Fatalf("unable to parse file name by %s, please specify <output file>", args[0])
+				logger.Fatalf("unable to parse file name by %s, please specify <output file>", target)
 			} else {
 				logger.Debugf("parse file name %s by URL path", name)
-				args = append(args, name)
+				output = name
 			}
 		}
-
 		overwrite, _ := cmd.Flags().GetBool("overwrite")
-		if info, err := os.Stat(args[1]); err == nil {
+		if info, err := os.Stat(output); err == nil {
 			if info.IsDir() {
-				logger.Fatalf("%s is a directory", args[1])
+				logger.Fatalf("%s is a directory", output)
 			}
 			if !overwrite {
-				logger.Fatalf("%s already exists, you should use --overwrite", args[1])
+				logger.Fatalf("%s already exists, you should use --overwrite", output)
 			}
 		}
 		resolveArr, _ := cmd.Flags().GetStringArray("resolve")
@@ -73,7 +73,7 @@ var fetchCmd = &cobra.Command{
 		if header.Get("Cookie") == "" {
 			header.Set("Cookie", cookie)
 		}
-		logger.Debugf("url: %s  insecure: %v  enable HTTP2: %v  disallow redirects: %v proxy: %s", args[0], insecure, enableHTTP2, noRedirect, proxyURL)
+		logger.Debugf("url: %s  insecure: %v  enable HTTP2: %v  disallow redirects: %v proxy: %s", target, insecure, enableHTTP2, noRedirect, proxyURL)
 		logger.Debugf("header: %+v", header)
 		logger.Debugf("resolve host map: %v", resolveHostMap)
 		logger.Debugf("specify CA certificate: %v", certPool != nil)
@@ -103,25 +103,25 @@ var fetchCmd = &cobra.Command{
 				fetcher.Header.Add(key, v)
 			}
 		}
-		supported, length, err := fetcher.Inspect(args[0])
+		supported, length, err := fetcher.Inspect(target)
 		if err != nil {
 			logger.Fatalln(err)
 		}
 		if !supported {
 			logger.Warnln("not support ranges")
 		}
-		fw, err := fetch.NewFileWriter(args[1])
+		fw, err := fetch.NewFileWriter(output)
 		if err != nil {
 			logger.Fatalln(err)
 		}
 		defer fw.Close()
 		bar := newBar(length,
-			progressbar.OptionSetDescription("Downloading [cyan]"+args[1]+"[reset]..."),
+			progressbar.OptionSetDescription("Downloading [cyan]"+output+"[reset]..."),
 		)
 		fw.OnWrite(func(n int, index int, start, end, length int64) {
 			bar.Add(n)
 		})
-		if err := fetcher.DownloadWithManual(args[0], supported, length, &fetch.DownloadOption{
+		if err := fetcher.DownloadWithManual(target, supported, length, &fetch.DownloadOption{
 			Concurrency: concurrency,
 			HookContext: fw.HookContext,
 		}); err != nil {
@@ -141,13 +141,14 @@ func init() {
 	// 此外可能出现"stream error: stream ID 5; INTERNAL_ERROR; received from peer" 的错误
 	fetchCmd.Flags().Bool("http2", false, "enable HTTP2 when uploading or downloading")
 	fetchCmd.Flags().Bool("no-redirect", false, "disallow redirects")
-	fetchCmd.Flags().String("proxy", "", "proxy url")
+	fetchCmd.Flags().StringP("proxy", "p", "", "proxy url")
 	fetchCmd.Flags().StringArray("resolve", []string{}, "resolve host, * for all, eg. example.com:127.0.0.1  *:127.0.0.1")
 	fetchCmd.Flags().BoolP("insecure", "k", false, "insecure skip verify")
 	fetchCmd.Flags().String("cacert", "", "CA certificate path")
 	fetchCmd.Flags().IntP("concurrency", "c", 12, "number of concurrent goroutines")
 	fetchCmd.Flags().StringArrayP("header", "H", []string{}, "header, example: -H \"Cookie:a=1\"")
 	fetchCmd.Flags().StringP("cookie", "C", "", "cookie, example: -C \"a=1\"")
+	fetchCmd.Flags().StringP("output", "o", "", "output file")
 	fetchCmd.Flags().Bool("overwrite", false, "overwrite")
 	rootCmd.AddCommand(fetchCmd)
 }
